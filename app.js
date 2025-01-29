f7bdb4a6c0ccf463f5d1413a3f88a0c0e124e509
CarpinteroCreativo <61610433+CarpinteroCreativo@users.noreply.github.com>
2025-01-29 13:09:19 -0600

    Create app.js

---
+++ app.js
@@ -0,0 +1,17 @@
+
+//Dias        D      L     M
+let monto1 = 546 + 1718 + 443;
+
+//Dias        M      J     V
+let monto2 = 0 + 0 + 0;
+
+//Dias        S
+let monto3 = 0;
+
+let montoSemana = monto1 + monto2 + monto3;
+
+document.write(`<h1>
+Saldo</h1>
+<p>${montoSemana}</p>`
+
+);
