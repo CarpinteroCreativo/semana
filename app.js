1a0967fb046e02d2006a2457cb20cadd9df70de7
CarpinteroCreativo <61610433+CarpinteroCreativo@users.noreply.github.com>
2025-02-16 12:02:01 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 
-//Dias        D      L   M
+//Dias        D      L      M
 let monto1 = 450 + 2017 + 491;
 
-//Dias       M   J   V
-let monto2 = 1480 + 0 + 0;
+//Dias       M       J     V
+let monto2 = 1480 + 31 + 170;
 
-//Dias       S
-let monto3 = 0;
+//Dias         S
+let monto3 = 1749;
 
 let montoSemana = monto1 + monto2 + monto3;
 
